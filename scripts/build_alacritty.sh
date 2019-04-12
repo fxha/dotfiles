5177670c1ab89fccb28ae9af606ef13eed8c7f78
Felix Häusler <hfelix@protonmail.ch>
2019-04-12 20:28:15 +0200

    fix alacritty build

--- scripts/build_alacritty.sh
+++ scripts/build_alacritty.sh
@@ -7,8 +7,6 @@ set -e
 
 echo " [-] Building alacritty"
 
-#cargo install --git https://github.com/jwilm/alacritty
-
 mkdir -p /tmp/dotfiles
 pushd /tmp/dotfiles > /dev/null
 git clone https://github.com/jwilm/alacritty.git &>> "$DOTFILES_LOG_FILE"
@@ -16,7 +14,7 @@ git clone https://github.com/jwilm/alacritty.git &>> "$DOTFILES_LOG_FILE"
 pushd alacritty > /dev/null
 echo "   This may take a while..."
 cargo build --release &>> "$DOTFILES_LOG_FILE"
-cargo install &>> "$DOTFILES_LOG_FILE"
+cargo install --force --path . &>> "$DOTFILES_LOG_FILE"
 
 # macOS
 #make app
